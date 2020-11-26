177f7459f463b521693f4e3d8c8634a438a472ba
Marìa Carolina Gòmez <mayravargase01@gmail.com>
2020-11-25 19:00:55 -0500

    js-25-11-2020

---
+++ main.js
@@ -0,0 +1,17 @@
+// let lista_informacion = ["Mayra Alejandra Vargas"];
+// let nombre = prompt("ingrese su nombre");
+// lista_informacion;
+// console.log(lista_informacion);
+
+
+// let lista_informacion = [];
+// lista_informacion["nombre"] = [" Mayra A."];
+
+// let nombre = prompt("ingrese su nombre");
+// lista_informacion.nombre = nombre
+// let apellido = prompt("ingrese apellido");
+// lista_informacion.APELLIDO = apellido;
+
+
+
+
